b2bf21354d7b5fa24f8d970158144e05fc7cbe00
Timofey Kondrashin <tkondrashin@icloud.com>
2026-01-23 14:36:27 +0300

    feat(nvim): Improve speed

--- nvim/lua/plugins/editor/dadbod.lua
+++ nvim/lua/plugins/editor/dadbod.lua
@@ -1,12 +1,14 @@
 return {
   {
-    "tpope/vim-dadbod",
+    'tpope/vim-dadbod',
     lazy = true,
+    event = 'VeryLazy',
   },
   {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = { "tpope/vim-dadbod" },
-    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    'kristijanhusak/vim-dadbod-ui',
+    event = 'VeryLazy',
+    dependencies = { 'tpope/vim-dadbod' },
+    cmd = { 'DBUI', 'DBUIToggle', 'DBUIAddConnection', 'DBUIFindBuffer' },
     init = function()
       -- Не открывать help-таб при запуске
       vim.g.db_ui_use_nerd_fonts = 1
@@ -14,15 +16,16 @@ return {
     end,
   },
   {
-    "kristijanhusak/vim-dadbod-completion",
-    ft = { "sql", "mysql", "plsql" },
-    dependencies = { "hrsh7th/nvim-cmp" },
+    'kristijanhusak/vim-dadbod-completion',
+    event = 'VeryLazy',
+    ft = { 'sql', 'mysql', 'plsql' },
+    dependencies = { 'hrsh7th/nvim-cmp' },
     config = function()
       -- Включаем cmp-dadbod
-      require("cmp").setup.filetype({ "sql" }, {
+      require('cmp').setup.filetype({ 'sql' }, {
         sources = {
-          { name = "vim-dadbod-completion" },
-          { name = "buffer" },
+          { name = 'vim-dadbod-completion' },
+          { name = 'buffer' },
         },
       })
     end,
