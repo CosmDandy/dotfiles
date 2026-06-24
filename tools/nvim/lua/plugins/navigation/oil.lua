0f2271292632edf936c951d6db1a60b81ba5aa10
Timofey Kondrashin <tkondrashin@icloud.com>
2026-06-24 01:27:33 +0000

    feat(nvim): oil auto-refresh on external changes
    Enable experimental_watch_for_changes so oil reflects files created/
    removed in terminal or via git checkout without reopening.

--- tools/nvim/lua/plugins/navigation/oil.lua
+++ tools/nvim/lua/plugins/navigation/oil.lua
@@ -3,9 +3,11 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {
+    -- следить за изменениями директории (создание/удаление файлов в терминале, git checkout)
+    experimental_watch_for_changes = true,
     view_options = {
       show_hidden = true,
-    }
+    },
   },
   dependencies = {
     { 'echasnovski/mini.icons', opts = {} },
